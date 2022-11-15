36f844dcc387831e4ea89fa0825b3c6ee7abfded
Eduardo Oliveira <eduardoykky@gmail.com>
2022-11-15 01:27:31 -0300

    terceiro commit

--- 22ListaBiblio.js
+++ 22ListaBiblio.js
@@ -1,6 +1,6 @@
 
 let autores = ["Oda" + "Kishimoto" + "Araki" + "Fujimoto" + "Q Hayashida" + "Kentaro"]
-let anos = [1999 + 2000 + 1980 + 2019 + 2011 + 1995]
+let anos = [1999 + 2000 + 1980 + 2019 + 2011 + 1988]
 let nomes = ["One Piece" + "Naruto" + "JoJo" + "ChainsawMan" + "Dorohedoro" + "Berserk"]
 let idiomas = ["português, inglês" + "português" + "português, espanhol" + "português, inglês, japonês" + "inglês, espanhol" + "japonês, inglês"]
 
@@ -17,6 +17,26 @@ function BuscarAutor() {
         console.log(nomes[x])
     })
 }
+function BuscarAno() {
+    let autor = prompt("Insira o nome do autor para buscar.")
+    let ano = parseInt(prompt("Insira a partir de que ano deseja buscar."))
+    let indexAutores = 0
+    let contador = 0
+    autores.forEach(x =>{
+        if (autor == x && anos[contador] > ano) {
+            indexAutores.push(contador)
+        }
+        contador++
+    })
+    indexAutores.forEach(x => console.log(nomes[x]))
+}
+
+function BuscarVersoes() {
+    let nome = prompt("Insira o nome do livro que deseja pesquisar")
+    let index = nomes.indexOf(nome)
+    console.log(idiomas[index])
+}
+
 function OrdernarAno() {
     let anosSup = []
     let nomesSup = []
